3451e96a181d63bf7cdcc4c2c22a9d896912aa29
Gabriel Vitebo <vitebo@outlook.com>
2023-01-08 21:55:58 -0300

    initial commit

---
+++ js/data.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    title: "The Turning Point",
+    artist: "Lacrimosa",
+    cover: "album-elodia.jpg",
+    file: "The-Turning-Point.mp3",
+  },
+  {
+    title: "Vent'anni",
+    artist: "Maneskin",
+    cover: "album-ventanni.jpg",
+    file: "VENT'ANNI.mp3",
+  },
+  {
+    title: "Happier Than Ever",
+    artist: "Billie Eilish",
+    cover: "album-HappierThanEver.jpg",
+    file: "Happier-Than-Ever.mp3",
+  },
+  {
+    title: "Want You Bad",
+    artist: "The Offspring",
+    cover: "album-conspiracyOfOne.jpg",
+    file: "Want-You-Bad.mp3",
+  },
+  {
+    title: "Basket Case",
+    artist: "Dookie",
+    cover: "album-dookie.jpg",
+    file: "Basket-case.mp3",
+  },
+  {
+    title: "Teenagers",
+    artist: "My Chemical Romance",
+    cover: "album-TheBlackParade.jpg",
+    file: "Teenagers.mp3",
+  },
+  {
+    title: "Do I Wanna Know?",
+    artist: "Arctic Monkeys",
+    cover: "album-AM.jpg",
+    file: "Do-I-Wanna-Know.mp3",
+  },
+]
